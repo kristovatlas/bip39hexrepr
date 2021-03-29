23c05496af129eec24b7c97f70811cbff897bf8f
Kristov Atlas <author@anonymousbitcoinbook.com>
2021-03-29 11:49:42 -0500

    init
    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # bip39hexrepr
+
 Convert BIP39 phrase to succinct hex format and vice versa
+
+## Usage
+
+```
+Usage:
+
+$ python3 bip39_hex_convert.py "exile credit explain garden question problem such virtual pride caution real health until still wire calm title famous return practice panic culture jungle clinic"
+exile credit explain garden question problem such virtual pride caution real health until still wire calm title famous return practice panic culture jungle clinic
+=>
+27d1982842fd57c55b6c37a35541255973517716ae7e11047172955c154b4fd1ac3c8159
+```
+
+```
+$ python3 bip39_hex_convert.py "27d1982842fd57c55b6c37a35541255973517716ae7e11047172955c154b4fd1ac3c8159"
+27d1982842fd57c55b6c37a35541255973517716ae7e11047172955c154b4fd1ac3c8159
+=>
+exile credit explain garden question problem such virtual pride caution real health until still wire calm title famous return practice panic culture jungle clinic
+```
---
+++ bip39_hex_convert.py
@@ -0,0 +1,120 @@
+"""Convert BIP39 phrase to succinct hex format and vice versa
+
+May be useful for tools that accept hex format such as:
+https://github.com/trezor/python-shamir-mnemonic/
+
+As the wordlist is 2048 words, each word corresponds to the value 0 to 2047
+in decimal or 0x000 to 0x7ff in hex. Therefore the word "abandon" can be
+encoded more succinctly as '000' rather than:
+>>> 'abandon'.encode('utf-8').hex()
+'6162616e646f6e'
+"""
+import sys
+
+WORDLIST = 'english.txt'
+
+def load_wordlist_val_map():
+    """BIP39 word => position in wordlist (0-based)"""
+    wordlist_val_map = dict()
+    with open(WORDLIST) as wordlist:
+        word_file = wordlist.readlines()
+        position = 0
+        for word in word_file:
+            word = word.strip()
+            wordlist_val_map[word] = position
+            position += 1
+    assert len(wordlist_val_map) == 2048
+    assert wordlist_val_map['abandon'] == 0
+    assert wordlist_val_map['zoo'] == 2047
+    return wordlist_val_map
+
+def load_val_wordlist_map():
+    """Position in wordlist (0-based) => BIP39 word"""
+    val_word_map = dict()
+    with open(WORDLIST) as wordlist:
+        word_file = wordlist.readlines()
+        position = 0
+        for word in word_file:
+            word = word.strip()
+            val_word_map[position] = word
+            position += 1
+    assert len(val_word_map) == 2048
+    assert val_word_map[0] == 'abandon'
+    assert val_word_map[2047] == 'zoo'
+    return val_word_map
+
+def get_hex_chunks(hex_str):
+    """From one hex str return it in 3 chars at a time"""
+    return [hex_str[i:i+3] for i in range(0, len(hex_str), 3)]
+
+def hex_to_phrase(hex_str):
+    """Convert hex representation to BIP39 phrase using english word list"""
+    assert len(hex_str) % 3 == 0
+
+    words = []
+    val_wordlist_map = load_val_wordlist_map()
+
+    for hex_word in get_hex_chunks(hex_str):
+        int_word = int(hex_word, 16)
+        word = val_wordlist_map[int_word]
+        words.append(word)
+
+    return " ".join(words)
+
+def phrase_to_hex(phrase):
+    """Convert BIP39 english word phrase to hex representation"""
+    wordlist_val_map = load_wordlist_val_map()
+    output = ''
+    words = phrase.split(' ')
+    for word in words:
+        int_val = wordlist_val_map[word]
+        hex_val = hex(int_val)[2:].zfill(3)
+        output += hex_val
+
+    return output
+
+def phrase_to_hex2(phrase):
+    """Convert BIP39 english word phrase to hex represenation
+
+    This is a v2 that first converts integer value  in [0, 2047] to binary and
+    then to hex, saving a small amount of space. This condensed a sample 24-word
+    passphrase to 64 hex characters instead of the 72 characters of the original
+    version of this function. However, since it is a less intuitive way of
+    representing the hex phrase should someone in the future need to reverse
+    engineer how the conversion worked, and since the space savings are small, I
+    leave it as dead code for now.
+    """
+    wordlist_val_map = load_wordlist_val_map()
+    words = phrase.split(' ')
+    binstring = ''
+
+    for word in words:
+        int_val = wordlist_val_map[word]
+        binstring += "{0:b}".format(int_val)
+
+    hex_str = hex(int(binstring, 2))[2:]
+
+    #SatoshiLabs shamir requires input of even number of bytes as "master-secret"
+    missing_padding = len(hex_str) % 4
+    #arbitrarily left pad with zeroes
+    hex_str = "0" * missing_padding + hex_str
+    return hex_str
+
+def _main(argv):
+    print(argv[1])
+    print("=>")
+    try:
+        int(argv[1], 16)
+
+        phrase = hex_to_phrase(argv[1])
+        print(phrase)
+
+    except ValueError:
+        output = phrase_to_hex(argv[1])
+        print(output)
+
+        #output2 = phrase_to_hex2(argv[1])
+        #print(output2)
+
+if __name__ == '__main__':
+    _main(sys.argv)
